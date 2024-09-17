6a4ceb38070186a343e13c5bce3ad6ee65cf54ea
Srđan Filipović <srdjanfilipovic991@gmail.com>
2024-09-17 14:22:41 +0200

    Dohvaćanje podataka od korisnika

--- Frontend/src/pages/korisnici/KorisniciPregled.jsx
+++ Frontend/src/pages/korisnici/KorisniciPregled.jsx
@@ -1,12 +1,54 @@
-import { Container } from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
+import KorisnikService from "../../services/KorisnikService";
+import { useEffect, useState } from "react";
 
 export default function KorisniciPregled(){
+
+
+    const[korisnici,setKorisnici] = useState();
+
+
+    async function dohvatiKorisnike(){
+        
+        const podaci = await KorisnikService.get()
+        .then((odgovor) => {
+            console.log('Vratio se')
+            console.log(odgovor);
+            setKorisnici(odgovor);
+            
+        })
+        .catch((e) => {console.log(e)});
+
+        
+    }
+
+    useEffect(() => {
+        dohvatiKorisnike();
+    },[]);
 
 
 
     return(
         <Container>
-            Pregled korisnika
+            <Table striped bordered hover responsive>
+                <thead>
+                    <tr>
+                        <th>Ime</th>
+                        <th>Prezime</th>
+                        <th>Email</th>
+                        <th>Akcija</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {korisnici && korisnici.map((korisnik,index) => (
+                        <tr key = {index}>
+                            <td>{korisnik.ime}</td>
+                            <td>{korisnik.prezime}</td>
+                            <td>{korisnik.email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </Container>
     )
 }
