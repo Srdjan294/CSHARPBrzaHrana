55ba1653a13aef45ed3656f50945d3c8d9cccf3b
Srđan Filipović <srdjanfilipovic991@gmail.com>
2024-09-24 13:17:53 +0200

    Male promjene

--- Frontend/src/components/NavbarBrzaHrana.jsx
+++ Frontend/src/components/NavbarBrzaHrana.jsx
@@ -17,7 +17,7 @@ export default function NavbarBrzaHrana(){
     
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Brza Hrana</Navbar.Brand>
+        <Navbar.Brand onClick={() => navigate(RoutesNames.HOME)}>Brza Hrana</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- Frontend/src/pages/Pocetna.jsx
+++ Frontend/src/pages/Pocetna.jsx
@@ -1,11 +1,15 @@
 import { Container } from "react-bootstrap";
 
 
-
 export default function Pocetna(){
     return(
         <Container>
-            Dobro došli kasnije će biti nešto pametnije ovdje
+            
+            <div class="text-container">
+            <h1>Dobro došli</h1>
+            <p>Ovo je moja aplikacija za naručivanje fast-food-a</p>
+            </div>
+            
         </Container>
     );
 }
